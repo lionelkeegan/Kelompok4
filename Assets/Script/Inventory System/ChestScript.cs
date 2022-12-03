42df36e6f3d091f3a5f2baad67604abc26d78a2a
Azraqisy123 <107374487+Azraqisy123@users.noreply.github.com>
2022-12-03 20:05:48 +0700

    Update Bug

--- Assets/Script/Inventory System/ChestScript.cs
+++ Assets/Script/Inventory System/ChestScript.cs
@@ -2,12 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ChestScript : InventoryUI
-{
+public class ChestScript : MonoBehaviour {
     // Start is called before the first frame update
-    void Awake()
+    [SerializeField] GameObject slotPrefab;
+    [SerializeField] int numOfSlots = 5;
+
+    List<RectTransform> slots = new List<RectTransform>();
+
+    private void Start()
     {
-        
+        BuildSlots();
+    }
+    
+    private void BuildSlots()
+    {
+        Transform[] children = transform.GetComponentsInChildren<Transform>();
+        foreach (var trans in children)
+        {
+            if (trans == this.transform)
+                continue;
+            Destroy(trans.gameObject);
+        }
+
+        slots.Clear();
+        for (int i = 0; i < numOfSlots; i++)
+        {
+           var slot = Instantiate(original: slotPrefab, parent: this.transform);
+           slots.Add(slot.GetComponent<RectTransform>());
+        }
     }
 
 }
